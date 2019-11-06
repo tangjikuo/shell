bc0eb8c2f7e7a33d82f2ca2264d4e313df3bee37
tangjikuo <351480441@qq.com>
2019-11-06 23:56:39 +0800

    change mysql连接
    change mysql username and password

--- mysql数据库/连接数据库并发送命令.sh
+++ mysql数据库/连接数据库并发送命令.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 #连接数据库
-mysql=`which mysql
-`
+mysql=`which mysql`
 #发送单个命令
-$mysql emwjs -u test -e "show databases;"
+$mysql -p Tjk@2019 -u root -e "show databases;"
 
 #发送多个命令
-$mysql emwjs -u test <<EOF
+$mysql -p Tjk@2019 -u root <<EOF
 show tables;
 select * from em_admin;
 EOF
